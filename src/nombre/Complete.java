765e9b7b0f8326b00f2b7298f799c224df257131
Kelly <ceda_4@hotmail.com>
2019-03-08 20:26:22 -0600

    Adding fisrt file

---
+++ src/nombre/Complete.java
@@ -0,0 +1,27 @@
+package nombre;
+
+import java.util.Scanner;
+
+public class Complete {
+
+	 Scanner sc;
+	 int cant;
+	private String nom;
+
+	public int getCant() {
+		return cant;
+	}
+
+	public void setCant(int cant) {
+		this.cant = cant;		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNombre(String nom) {
+		this.nom = nom;
+	}
+
+}
---
+++ src/nombre/Imprimirnombre.java
@@ -0,0 +1,34 @@
+package nombre;
+
+import java.util.Scanner;
+
+public class Imprimirnombre {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("cuantas palabras tiene tu nombre?");	
+		int cant = sc.nextInt();
+		
+		String arreglo [] = new String[cant];
+		String aux;	
+		
+		for(int i=0; i<arreglo.length;i++) {	
+			
+
+			System.out.println("escribe nombre: " + i + arreglo[i]);
+			aux = sc.next();
+			arreglo[i]= aux;		
+	}	
+		for(int i=0; i<arreglo.length; i++) 
+		{
+			
+		System.out.println("Te llamas"+ i + ":" + arreglo[i] );	
+		
+			}
+		sc.close();
+			
+	
+}
+}
